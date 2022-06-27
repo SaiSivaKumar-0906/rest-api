bebefd888c982708ef56f33dee2b597a3c2b958c
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-06-27 21:22:41 +0530

    Add files via upload

---
+++ api/data.js
@@ -0,0 +1,23 @@
+const js = [
+    {
+        "id":1,
+        "first-month":"no struggle thinks everything easy",
+    },
+    {
+        "id":2,
+        "second-month":"ok it's hard, but not very hard"
+    },
+    {
+        "id":3,
+        "third-month":"addicited to tutorial hell"
+    },
+    {
+        "id":4,
+        "fourt-month":"realisation in tutorial hell"
+    },
+    {
+        "id":5,
+        "fuck":"typing words is so annoying"
+    }
+]
+module.exports = js;
